b34e9b70bb3a48f30d5faf6dbfe7c2721b4ae7e7
andersonnw <and14094@byui.edu>
2019-05-23 10:31:28 -0600

    Added the Read file functionality

--- MegaDesk/MegaDesk/DeskQuote.cs
+++ MegaDesk/MegaDesk/DeskQuote.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace MegaDesk
         private const decimal ROSEWOOD_COST = 200.0M;
         private const decimal VENEER_COST = 125.0M;
 
+        private int[,] _shipPrice;
 
         public string customerName;
         public Desk desk;
@@ -28,9 +30,36 @@ namespace MegaDesk
         public decimal quotePrice;
         public Shipping shipping;
 
+        //read the price txt file in and save it to the array
+        private void getRushOrder()
+        {
+            _shipPrice = new int[3, 3];
+
+            var file = @"rushOrderPrices.txt";
+            string[] temp = new string[9];
+            try
+            {
+                temp = File.ReadAllLines(file);
+                int count = 0;
+                for (int i = 0; i < 3; ++i)
+                {
+                    for (int j = 0; j <3; ++j)
+                    {
+                        _shipPrice[i, j] = Convert.ToInt32(temp[count]);
+                        count++;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                return;
+            }
+        }
+
         //Calcuates the quote Price;
         public void setPrice()
         {
+            getRushOrder();
             decimal price = BASE_DESK_PRICE;
 
             //check the area of the desk and charge accordingly
@@ -78,23 +107,23 @@ namespace MegaDesk
                     }
                 case Shipping.rush3:
                     {
-                        if(surfaceArea < 1000) { price += 60; }
-                        else if(surfaceArea <= 2000) { price += 70; }
-                        else { price += 80; }
+                        if(surfaceArea < 1000) { price += _shipPrice[0, 0]; }
+                        else if(surfaceArea <= 2000) { price += _shipPrice[0, 1]; }
+                        else { price += _shipPrice[0, 2]; }
                         break;
                     }
                 case Shipping.rush5:
                     {
-                        if (surfaceArea < 1000) { price += 40; }
-                        else if (surfaceArea <= 2000) { price += 50; }
-                        else { price += 60; }
+                        if (surfaceArea < 1000) { price += _shipPrice[1, 0]; }
+                        else if (surfaceArea <= 2000) { price += _shipPrice[1, 1]; }
+                        else { price += _shipPrice[1, 2]; }
                         break;
                     }
                 case Shipping.rush7:
                     {
-                        if (surfaceArea < 1000) { price += 30; }
-                        else if (surfaceArea <= 2000) { price += 35; }
-                        else { price += 40; }
+                        if (surfaceArea < 1000) { price += _shipPrice[2, 0]; }
+                        else if (surfaceArea <= 2000) { price += _shipPrice[2, 1]; }
+                        else { price += _shipPrice[2, 2]; }
                         break;
                     }
             }
